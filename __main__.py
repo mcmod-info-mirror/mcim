fcb16dd83dec541cbb7004179baf5ab091e7b1ea
zyxkad <zyxkad@gmail.com>
2022-07-16 09:23:12 -0600

    add entry point

---
+++ __main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import mcim
+
+def main():
+	mcim.main()
+
+if __name__ == '__main__':
+	main()
--- apis/__init__.py
+++ apis/__init__.py
@@ -1,16 +1,10 @@
 
-from . import base
-from . import curseforge
-from . import modrinth
+from .base import *
+from .curseforge import *
+from .modrinth import *
 
-__all__ = []
-
-def export_pkg(pkg):
-	if hasattr(pkg, '__all__'):
-		globals()['__all__'].extend(pkg.__all__)
-		for n in pkg.__all__:
-			globals()[n] = getattr(pkg, n)
-
-export_pkg(base)
-export_pkg(curseforge)
-export_pkg(modrinth)
+__all__ = [
+	'StatusCodeException',
+	'CurseForgeApi',
+	'ModrinthApi'
+]
