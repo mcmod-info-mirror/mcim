512e89b99e8062a20cb83e38d28027e4035c1f5c
zyxkad <zyxkad@gmail.com>
2022-07-17 12:09:29 -0600

    update async_httpclient.py

--- async_httpclient.py
+++ async_httpclient.py
@@ -10,73 +10,46 @@ class AsyncHTTPClient:
 	'''
 	Usage: cli = AsyncHTTPClient()
 	'''
-	def __init__(self): # limit: int = 64 TODO
-		pass
+	def __init__(self, **kwargs): # limit: int = 64 TODO
+		self._session = None
+		self._session_kwargs = kwargs
 
-	async def new_session(self, headers=None, timeout=None):
+	@property
+	def session(self):
+		return self._session
+
+	async def new_session(self, **kwargs):
 		'''
 		Usage:
-			async with cli.new_session() as session:
+			async with await cli.new_session() as session:
 				pass
 		'''
-		return aiohttp.ClientSession(headers=headers, timeout=aiohttp.ClientTimeout(total=timeout))
+		return aiohttp.ClientSession(**{**self._session_kwargs, **kwargs})
 
-	async def get_session(self,headers=None, timeout=None):
+	async def get_session(self, **kwargs):
 		'''
 		Usage:
-			async with cli.get_session() as session:
-				pass
+			session = await cli.get_session()
 		'''
-		return await self.new_session(headers=headers, timeout=timeout)
+		if self._session is None:
+			self._session = await self.new_session(**kwargs)
+		return self._session
 
-	async def get(self, url: str, headers=None, timeout=None, sem=None, session=None, *, callback=None):
+	async def get(self, url: str, /, *, callback=None, **kwargs):
 		'''
 		Usage: res, content = await cli.get('http://example.com')
 		'''
-		if sem is not None:
-			async with sem:
-				if session is None:
-					pass
-					async with await self.get_session(headers, timeout=timeout) as session:
-						async with session.get(url) as res:
-							if callback is not None:
-								return await callback(res)
-							reader = res.content
-							content = await reader.read()
-							return res, content
-				else:
-					pass
-					async with session.get(url) as res:
-						pass
-						if callback is not None:
-							return await callback(res)
-						reader = res.content
-						content = await reader.read()
-						return res, content
-		else:
-			if session is None:
-				pass
-				async with await self.get_session(headers, timeout=timeout) as session:
-					async with session.get(url) as res:
-						if callback is not None:
-							return await callback(res)
-						reader = res.content
-						content = await reader.read()
-						return res, content
-			else:
-				pass
-				async with session.get(url) as res:
-					pass
-					if callback is not None:
-						return await callback(res)
-					reader = res.content
-					content = await reader.read()
-					return res, content
-			# 感觉这个session的判断不好但不知道咋改
-			# '''**想不出建议不改 \o/**'''
+		async with await self.get_session() as session:
+			async with session.get(url, **kwargs) as res:
+				if callback is not None:
+					return await callback(res)
+				reader = res.content
+				content = await reader.read()
+				return res, content
 
-	async def get_all(self, urls: list[str], headers=None, timeout=None, session=None, sem=None, *, callback=None):
-		sem = asyncio.Semaphore(sem)
+	async def get_all(self, urls: list[str], *, limit: int=-1, callback=None, **kwargs):
+		if limit > 0:
+			kwargs['sem'] = asyncio.Semaphore(limit)
 		'''
 		Usage:
 			urls = ['http://example.com', 'http://example2.com']
@@ -84,15 +57,14 @@ class AsyncHTTPClient:
 			for i, (res, content) in enumerate(responses):
 				print('Response for "{}": {} ;Content: {} Byte'.format(urls[i], res.status, len(content)))
 		'''
-		tasks = [asyncio.create_task(self.get(url, headers, timeout=timeout, session=session, sem=sem, callback=callback)) for url in urls]
-		print(len(tasks))
+		tasks = [asyncio.create_task(self.get(url, callback=callback, **kwargs)) for url in urls]
 		return await asyncio.gather(*tasks)
 
-	# def get_all_sync(self, *args, **kwargs):
-	# 	'''
-	# 	Usage: responses = cli.get_all_sync(['http://example.com', 'http://www.example2.com'])
-	# 	'''
-	# 	return asyncio.run(self.get_all(*args, **kwargs))
+	def get_all_sync(self, *args, **kwargs):
+		'''
+		Usage: responses = cli.get_all_sync(['http://example.com', 'http://www.example2.com'])
+		'''
+		return asyncio.run(self.get_all(*args, **kwargs))
 
 class Tester:
 	def __init__(self):
@@ -129,5 +101,4 @@ class Tester:
 		tester.test_get_all_sync()
 
 if __name__ == '__main__':
-	# Tester.main() #偶尔误触运行了...
-	asyncio.run(AsyncHTTPClient().get('https://api.papermc.io/v2/projects',callback=lambda res: print(res.status)))
+	Tester.main()
