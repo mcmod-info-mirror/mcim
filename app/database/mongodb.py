2685a5555a5f7f798ec4b06945028d5e384bdf6f
z0z0r4 <z0z0r4@outlook.com>
2024-06-02 07:10:04 +0800

    fix: add auth
    All your data is backed up. You must pay 0.0059 BTC to 1GbLiucJ7fhsM3sYrPKHvZ5mUW2p4AYW7p In 48 hours, your data will be publicly disclosed and deleted. (more information: go to http://iplis.ru/data01)After paying send mail to us: dzen+1jqfmv@onionmail.org and we will provide a link for you to download your data.

--- app/database/mongodb.py
+++ app/database/mongodb.py
@@ -22,7 +22,7 @@ def init_mongodb_syncengine() -> SyncEngine:
     if sync_mongo_engine is None:
         sync_mongo_engine = SyncEngine(
             client=MongoClient(
-                f"mongodb://{_mongodb_config.host}:{_mongodb_config.port}" # {_mongodb_config.user}:{_mongodb_config.password}@
+                f"mongodb://{_mongodb_config.user}:{_mongodb_config.password}@{_mongodb_config.host}:{_mongodb_config.port}" # 
             ),
             database="mcim_backend",
         )
