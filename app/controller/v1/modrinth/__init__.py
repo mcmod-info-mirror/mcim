b51c933d493b7f7cb0a24aa89df1bf516e6cf104
z0z0r4 <z0z0r4@outlook.com>
2024-07-15 09:35:59 +0800

    fix: modrinth_mutil_file_update

--- app/controller/v1/modrinth/__init__.py
+++ app/controller/v1/modrinth/__init__.py
@@ -475,17 +475,18 @@ async def modrinth_mutil_file_update(request: Request, items: MultiUpdateItems):
         # check expire
         resp = {}
         project_ids_to_sync = set()
-        for original_hash, date, version_detail in versions_result:
-            resp[original_hash] = version_detail
-            if not (
-                datetime.strptime(
-                    version_detail["sync_at"], "%Y-%m-%dT%H:%M:%SZ"
-                ).timestamp()
-                + mcim_config.expire_second.modrinth.file
-                > time.time()
-            ):
-                project_ids_to_sync.add(version_detail["project_id"])
-                trustable = False
+        for version_result in versions_result:
+            for original_hash, date, version_detail in version_result:
+                resp[original_hash] = version_detail
+                if not (
+                    datetime.strptime(
+                        version_detail["sync_at"], "%Y-%m-%dT%H:%M:%SZ"
+                    ).timestamp()
+                    + mcim_config.expire_second.modrinth.file
+                    > time.time()
+                ):
+                    project_ids_to_sync.add(version_detail["project_id"])
+                    trustable = False
         if len(project_ids_to_sync) != 0:
             sync_multi_projects.send(project_ids=list(project_ids_to_sync))
             log.debug(f"Project {project_ids_to_sync} expired, send sync task.")
