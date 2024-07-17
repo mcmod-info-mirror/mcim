f3553f4c914cf67e9773c8c38a78efff3a4fe82f
z0z0r4 <z0z0r4@outlook.com>
2024-07-17 08:22:18 +0800

    fix: timezone

--- app/utils/loger/__init__.py
+++ app/utils/loger/__init__.py
@@ -5,6 +5,7 @@ import logging
 from types import FrameType
 from typing import cast
 from loguru import logger
+from datetime import datetime, timezone, timedelta
 
 from app.config import MCIMConfig
 
@@ -100,6 +101,14 @@ class InterceptHandler(logging.Handler):
             record.getMessage(),
         )
 
+def beijing(sec, what):
+    """日志返回北京时间的处理"""
+    utc_dt = datetime.now(timezone.utc) # 获取当前时间
+    beijing_time = utc_dt.astimezone(timezone(timedelta(hours=8))) # 转换为北京时间
+    return beijing_time.timetuple()
+
+# 日志时间改为北京时间
+logging.Formatter.converter = beijing
 
 Loggers = Logger()
 Loggers.init_config()
