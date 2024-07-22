7a918d976725ec0fa7ea69ba046790b3acbc9361
z0z0r4 <z0z0r4@outlook.com>
2024-07-22 17:40:07 +0800

    feat: add TimingMiddleware for logging request processing time

--- app/utils/middleware/__init__.py
+++ app/utils/middleware/__init__.py
@@ -1,3 +1,4 @@
 from app.utils.middleware.force_sync import ForceSyncMiddleware
+from app.utils.middleware.timing import TimingMiddleware
 
-__ALL__ = [ForceSyncMiddleware]
+__ALL__ = [ForceSyncMiddleware, TimingMiddleware]
