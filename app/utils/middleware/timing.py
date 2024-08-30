17e7ddf8669ee7ba85a1a7ec8f125838375b20b1
z0z0r4 <z0z0r4@outlook.com>
2024-08-30 20:42:32 +0800

    fix: log.warning

--- app/utils/middleware/timing.py
+++ app/utils/middleware/timing.py
@@ -18,7 +18,7 @@ class TimingMiddleware(BaseHTTPMiddleware):
         if route:
             route_name = route.name
             if process_time >= 10:
-                log.warn(f"{route_name} - {request.method} {request.url} {process_time:.2f}ms")
+                log.warning(f"{route_name} - {request.method} {request.url} {process_time:.2f}ms")
             else:
                 log.debug(f"{route_name} - {request.method} {request.url} {process_time:.2f}ms")
         return response
