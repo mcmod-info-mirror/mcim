e3ae613bf1b801aff9fed766e67a2ef23daa0a25
z0z0r4 <z0z0r4@outlook.com>
2024-09-16 20:08:48 +0800

    feat: sync file when file cdn not found in database

--- app/controller/file_cdn/__init__.py
+++ app/controller/file_cdn/__init__.py
@@ -82,6 +82,9 @@ if mcim_config.file_cdn:
                     headers={"Cache-Control": f"public, age={3600*24*7}"},
                     status_code=301,
                 )
+            else:
+                sync_project.send(project_id)
+                log.debug(f"sync project {project_id} task send.")
 
         file: Optional[mrFile] = await request.app.state.aio_mongo_engine.find_one(
             mrFile,
@@ -186,6 +189,9 @@ if mcim_config.file_cdn:
                     headers={"Cache-Control": f"public, age={3600*24*7}"},
                     status_code=301,
                 )
+            else:
+                sync_mutil_files.send([fileid])
+                log.debug(f"sync fileId {fileid} task send.")
 
         fileid = int(f"{fileid1}{fileid2}")
         file: Optional[cfFile] = await request.app.state.aio_mongo_engine.find_one(
