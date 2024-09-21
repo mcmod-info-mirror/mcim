456722a56ef82c5fa2435010a4100779a1843570
z0z0r4 <z0z0r4@outlook.com>
2024-09-21 22:05:22 +0800

    feat: ip forward

--- scripts/gunicorn_config.py
+++ scripts/gunicorn_config.py
@@ -13,3 +13,5 @@ os.environ["PROMETHEUS_MULTIPROC_DIR"] = "/tmp/prometheus_multiproc_dir"
 if os.path.exists("/tmp/prometheus_multiproc_dir"):
     shutil.rmtree("/tmp/prometheus_multiproc_dir")
 os.makedirs("/tmp/prometheus_multiproc_dir", exist_ok=True)
+
+access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
